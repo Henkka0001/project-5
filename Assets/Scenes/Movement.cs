a4c7caeccf6110d248694afa7499073f9f823849
Henri Kangas <Henri.Kangas01@gmail.com>
2018-06-14 10:19:45 +0300

    Project 5 tower defence
    Turret

---
+++ Assets/Scenes/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    Rigidbody rb;
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+
+}
+    void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(0, 11f, 0);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
